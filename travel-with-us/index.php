b48886903536bd8887b87d0fce37381292c3d855
blamerra <blamerra@gmail.com>
2016-12-15 23:22:49 -0500

    PHPRouter added

--- travel-with-us/index.php
+++ travel-with-us/index.php
@@ -1,1 +1,19 @@
- <?php echo 'Hello world'; ?>
+ <?php  
+require __DIR__ . '/vendor/autoload.php';
+
+
+ // Use this namespace
+use MiladRahimi\PHPRouter\Router;
+
+// Create brand new Router instance
+$router = new Router();
+
+// Map this function to home page
+$router->get("/php/travel-with-us/", function () {
+    return "This is home page!";
+});
+
+// Dispatch all matched routes and run!
+$router->dispatch();
+
+?>
